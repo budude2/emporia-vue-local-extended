59e3cac0bee8311f6d5a9ed9d358de497088350f
Thayne <legot1@gmail.com>
2025-12-29 13:51:44 -0700

    Update __init__.py

--- components/phase_config/__init__.py
+++ components/phase_config/__init__.py
@@ -1,70 +1,70 @@
 
 import esphome.codegen as cg
-import esphome.configvalidation as cv
+import esphome.config_validation as cv
 from esphome.components import sensor
-from esphome.const import CONFID
+from esphome.const import CONF_ID
 
-phaseconfigns = cg.esphomens.namespace("phaseconfig")
-PhaseConfig = phaseconfigns.class("PhaseConfig", cg.Component)
+phase_config_ns = cg.esphome_ns.namespace("phase_config")
+PhaseConfig = phase_config_ns.class_("PhaseConfig", cg.Component)
 
-CONFOVERALLVOLTAGE = "overallvoltage"
-CONFPHASEABVOLTAGE = "phaseabvoltage"
-CONFPHASEBCVOLTAGE = "phasebcvoltage"
-CONFPHASEACVOLTAGE = "phaseacvoltage"
-CONFPHASEAVOLTAGE = "phaseavoltage"
-CONFPHASEBVOLTAGE = "phasebvoltage"
-CONFPHASECVOLTAGE = "phasecvoltage"
+CONF_OVERALL_VOLTAGE = "overall_voltage"
+CONF_PHASE_AB_VOLTAGE = "phase_a_b_voltage"
+CONF_PHASE_BC_VOLTAGE = "phase_b_c_voltage"
+CONF_PHASE_AC_VOLTAGE = "phase_a_c_voltage"
+CONF_PHASE_A_VOLTAGE = "phase_a_voltage"
+CONF_PHASE_B_VOLTAGE = "phase_b_voltage"
+CONF_PHASE_C_VOLTAGE = "phase_c_voltage"
 
 CONFIGSCHEMA = cv.Schema(
     {
-        cv.GenerateID(): cv.declareid(PhaseConfig),
+        cv.GenerateID(): cv.declare_id(PhaseConfig),
 
         # Keep overall required as a reliable fallback
-        cv.Required(CONFOVERALLVOLTAGE): cv.useid(sensor.Sensor),
+        cv.Required(CONF_OVERALL_VOLTAGE): cv.useid(sensor.Sensor),
 
         # Others optional; if omitted, fallback behavior uses overall or NAN
-        cv.Optional(CONFPHASEABVOLTAGE): cv.useid(sensor.Sensor),
-        cv.Optional(CONFPHASEBCVOLTAGE): cv.useid(sensor.Sensor),
-        cv.Optional(CONFPHASEACVOLTAGE): cv.useid(sensor.Sensor),
+        cv.Optional(CONFPHASE_AB_VOLTAGE): cv.useid(sensor.Sensor),
+        cv.Optional(CONFPHASE_BC_VOLTAGE): cv.useid(sensor.Sensor),
+        cv.Optional(CONFPHASE_AC_VOLTAGE): cv.useid(sensor.Sensor),
 
-        cv.Optional(CONFPHASEAVOLTAGE): cv.useid(sensor.Sensor),
-        cv.Optional(CONFPHASEBVOLTAGE): cv.useid(sensor.Sensor),
-        cv.Optional(CONFPHASECVOLTAGE): cv.useid(sensor.Sensor),
+        cv.Optional(CONF_PHASE_A_VOLTAGE): cv.useid(sensor.Sensor),
+        cv.Optional(CONF_PHASE_B_VOLTAGE): cv.useid(sensor.Sensor),
+        cv.Optional(CONF_PHASE_C_VOLTAGE): cv.useid(sensor.Sensor),
     }
-).extend(cv.COMPONENTSCHEMA)
+).extend(cv.COMPONENT_SCHEMA)
 
-async def tocode(config):
+async def to_code(config):
     # Ensure helper functions are visible to lambdas
-    cg.addglobal(
-        cg.RawExpression('#include "esphome/components/phaseconfig/phaseconfig.h"')
+    cg.add_global(
+        cg.RawExpression('#include "esphome/components/phase_config/phase_config.h"')
     )
 
-    var = cg.newPvariable(config[CONFID])
-    await cg.registercomponent(var, config)
+    var = cg.new_Pvariable(config[CONFID])
+    await cg.register_component(var, config)
 
-    overall = await cg.getvariable(config[CONFOVERALLVOLTAGE])
-    cg.add(var.setoverallvoltage(overall))
+    overall = await cg.get_variable(config[CONF_OVERALL_VOLTAGE])
+    cg.add(var.set_overall_voltage(overall))
 
     if CONFPHASEABVOLTAGE in config:
-        ab = await cg.getvariable(config[CONFPHASEABVOLTAGE])
-        cg.add(var.setphaseabvoltage(ab))
+        ab = await cg.get_variable(config[CONFPHASE_AB_VOLTAGE])
+        cg.add(var.set_phase_a_b_voltage(ab))
 
     if CONFPHASEBCVOLTAGE in config:
-        bc = await cg.getvariable(config[CONFPHASEBCVOLTAGE])
-        cg.add(var.setphasebcvoltage(bc))
+        bc = await cg.get_variable(config[CONFPHASE_BC_VOLTAGE])
+        cg.add(var.set_phase_b_c_voltage(bc))
 
     if CONFPHASEACVOLTAGE in config:
-        ac = await cg.getvariable(config[CONFPHASEACVOLTAGE])
-        cg.add(var.setphaseacvoltage(ac))
+        ac = await cg.get_variable(config[CONFPHASE_AC_VOLTAGE])
+        cg.add(var.set_phase_a_c_voltage(ac))
 
     if CONFPHASEAVOLTAGE in config:
-        a = await cg.getvariable(config[CONFPHASEAVOLTAGE])
-        cg.add(var.setphaseavoltage(a))
+        a = await cg.get_variable(config[CONF_PHASE_A_VOLTAGE])
+        cg.add(var.set_phase_a_voltage(a))
 
     if CONFPHASEBVOLTAGE in config:
-        b = await cg.getvariable(config[CONFPHASEBVOLTAGE])
-        cg.add(var.setphasebvoltage(b))
+        b = await cg.get_variable(config[CONF_PHASE_B_VOLTAGE])
+        cg.add(var.set_phase_b_voltage(b))
 
     if CONFPHASECVOLTAGE in config:
-        c = await cg.getvariable(config[CONFPHASECVOLTAGE])
-        cg.add(var.setphasecvoltage(c))
+        c = await cg.get_variable(config[CONF_PHASE_C_VOLTAGE])
+        cg.add(var.set_phase_c_voltage(c))
