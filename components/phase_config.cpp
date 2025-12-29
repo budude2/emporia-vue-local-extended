2f2cae26442bdd2518538003c6af9d73d20c85da
Thayne <legot1@gmail.com>
2025-12-29 05:59:17 -0700

    Create phase_config.cpp

---
+++ components/phase_config.cpp
@@ -0,0 +1,10 @@
+// phase_config.h — helper for mapping CT phase strings to the correct voltage sensor
+#pragma once
+#include "phase_config.h"
+
+namespace esphome {
+namespace emporia_vue_extended {
+
+
+}
+}
