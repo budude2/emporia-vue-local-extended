a4b439f025f32a995e311ed89c22e0127cedd892
Thayne <legot1@gmail.com>
2025-12-29 12:18:41 -0700

    Update __init__.py

--- components/phase_config/__init__.py
+++ components/phase_config/__init__.py
@@ -1,1 +1,7 @@
+import esphome.codegen as cg
+import esphome.config_validataion as cv
 
+CONFIG_SCHEMA = cv.Schema({})
+
+def to_code (config):
+  cg.add_global(cg.RawExpression('#include "esphome/components/phase_config/phase_config.h"'))
